a425c1bc3b266d5603fe8f0146b67c46f750f8e5
vgbao1231 <vgbao1231@gmail.com>
2024-02-08 23:02:40 +0700

    first commit

---
+++ src/test/java/org/music_player/demo/MusicPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.music_player.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
