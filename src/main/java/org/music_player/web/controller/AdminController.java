c09cf0bd1c7fe09f903b3b538e09c458fa1d1fd0
vgbao1231 <vgbao1231@gmail.com>
2024-03-04 16:08:34 +0700

    Authenticate and authorize completed by vogiabao

--- src/main/java/org/music_player/web/controller/AdminController.java
+++ src/main/java/org/music_player/web/controller/AdminController.java
@@ -6,11 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
     @RequestMapping()
-    public String index() {
-        return "redirect:/admin/";
-    }
-    @RequestMapping("/")
     public String adminHomePage() {
         return "admin/index";
     }
